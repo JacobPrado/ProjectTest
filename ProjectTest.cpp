dcbab27e64609cea52753533a36ff1c8c18acf53
JacobPrado <jacob.prado@marquette.edu>
2021-11-30 13:41:43 -0600

    WaitCommEvent added

--- ProjectTest.cpp
+++ ProjectTest.cpp
@@ -1,13 +1,24 @@
-// ProjectTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+Project: BIEN 4280 Final Project
+Authors: Bob Dirmish, Nicohlas Ragonese, Sam Keller, Martin Boehm, Jacob Prado
+Description:Implement a serial communication between host PC and mbed device (Arduino nano 33)
+            to send the data received to Azure to use their speech-to-text services to use 
+            different sensors on the Arduino
+*/
 
-#include <stdio.h>
-#include <tchar.h>
 #include <Windows.h>
+#include <tchar.h>
+#include <assert.h>
+#include <stdio.h>
 
 int main()
 {
-    HANDLE hFile = CreateFile(
+    HANDLE hFile;
+    OVERLAPPED o;
+    BOOL fSuccess;
+    DWORD dwEvtMask;   
+    
+    hFile = CreateFile(
         L"COM6",
         GENERIC_READ | GENERIC_WRITE,
         NULL,
@@ -16,22 +27,62 @@ int main()
         FILE_ATTRIBUTE_NORMAL,
         NULL);
 
-    if (hFile) {
-        printf("CreateFile initialized.\n");
+    if (hFile == INVALID_HANDLE_VALUE) {
+        printf("CreateFile failed with error %d.\n", GetLastError());
+        return 1;
+    }
+    
+    //mask event
+
+    fSuccess = SetCommMask(hFile, EV_CTS | EV_DSR);
+
+    if (!fSuccess) {
+        printf("SetCommMask failed with error %d.\n", GetLastError());
+        return 1;
+    }
+    //event object for use by WaitCommEvent
+    o.hEvent = CreateEvent(
+        NULL,
+        TRUE,
+        FALSE,
+        NULL);
+
+    //initialize OVERLAPPED structure to zero
+    o.Internal = 0;
+    o.InternalHigh = 0;
+    o.Offset = 0;
+    o.OffsetHigh = 0;
+
+    assert(o.hEvent);
+
+    if (WaitCommEvent(hFile, &dwEvtMask, &o))
+    {
+        if (dwEvtMask & EV_DSR)
+        {
+            // To do.
+            printf("DSR\n");
+        }
+
+        if (dwEvtMask & EV_CTS)
+        {
+            // To do. 
+            printf("CTS\n");
+        }
+    }
+    else
+    {
+        DWORD dwRet = GetLastError();
+        if (ERROR_IO_PENDING == dwRet)
+        {
+            printf("I/O is pending...\n");
+
+            // To do.
+        }
+        else
+            printf("Wait failed with error %d.\n", GetLastError());
     }
 
-
+    CloseHandle(hFile); //closes the COM port
 
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
