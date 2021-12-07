4109b9faa08794dc6d2d648dac5b19a41b8269de
JacobPrado <jacob.prado@marquette.edu>
2021-12-07 12:39:13 -0600

    Stuck on ReadFile

--- ProjectTest.cpp
+++ ProjectTest.cpp
@@ -12,144 +12,51 @@ Description:Implement a serial communication between host PC and mbed device (Ar
 #include <strsafe.h>
 
 #define READ_TIMEOUT    500  //ms
-#define BUFFERSIZE      100
-DWORD g_BytesTransferred = 0;
+#define BUFFERSIZE      200
 
 //void DisplayError(LPTSTR lpszFunction);
 
-VOID CALLBACK FileIOCompletionRoutine(
-    __in  DWORD dwErrorCode,
-    __in  DWORD dwNumberOfBytesTransfered,
-    __in  LPOVERLAPPED lpOverlapped
-);
-
-VOID CALLBACK FileIOCompletionRoutine(
-    __in  DWORD dwErrorCode,
-    __in  DWORD dwNumberOfBytesTransfered,
-    __in  LPOVERLAPPED lpOverlapped)
-{
-    _tprintf(TEXT("Error code:\t%x\n"), dwErrorCode);
-    _tprintf(TEXT("Number of bytes:\t%x\n"), dwNumberOfBytesTransfered);
-    g_BytesTransferred = dwNumberOfBytesTransfered;
-}
-
 HANDLE hComm;
 DWORD dwBytesRead = 0;
-LPDWORD lpNBytesRead = 0;
-BOOL fWaitingOnRead = FALSE;
-OVERLAPPED ol = { 0 };
-LPOVERLAPPED LPol = { 0 };
-char ReadBuffer[BUFFERSIZE] = "Hello World!";
+BOOL succ = FALSE;
+char ReadBuffer[BUFFERSIZE] = { 0 };
 
 int main()
-{
-    
-
-    /*OVERLAPPED o;
-    BOOL fSuccess; 
-    DWORD dwEvtMask;*/
-    
+{    
     hComm = CreateFile(
         L"COM6",
         GENERIC_READ | GENERIC_WRITE,
-        0,
-        0,
-        OPEN_EXISTING,
         NULL,
-        0);
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL);
 
     if (hComm == INVALID_HANDLE_VALUE) {
         printf("CreateFile failed with error %d.\n", GetLastError());
         return GetLastError();
-    }
-    else {
+    }else{
         printf("CreateFile initialized successfully\n");
     }
-    while (1) {
 
-        if (FALSE == ReadFile(hComm, ReadBuffer, BUFFERSIZE - 1, &dwBytesRead, NULL))
-        {
+   // while (1) {
+    succ = ReadFile(hComm, ReadBuffer, BUFFERSIZE, &dwBytesRead, NULL);
+        if (!succ && (GetLastError() != ERROR_IO_PENDING)){
             //DisplayError(TEXT("ReadFile"));
-            printf("Terminal failure: Unable to read from file.\n GetLastError=%08x\n", GetLastError());
+            printf("Terminal failure: Unable to read from file.\nGetLastError=%08x\n", GetLastError());
             CloseHandle(hComm);
             return -1;
         }
-
-        SleepEx(5000, TRUE);
-
-        printf("Value in ReadBuffer: %s.\n", ReadBuffer);
-    }
+        
+       
+            printf("Value in ReadBuffer: %s.\n", ReadBuffer);
+        
+        
+        
+        //SleepEx(000, TRUE);
+    //}
     
-    /*dwBytesRead = g_BytesTransferred;
-    // This is the section of code that assumes the file is ANSI text. 
-    // Modify this block for other data types if needed.
-
-    if (dwBytesRead > 0 && dwBytesRead <= BUFFERSIZE - 1)
-    {
-        ReadBuffer[dwBytesRead] = '\0'; // NULL character
-
-        _tprintf(TEXT("Data read from COM6 (%d bytes): \n"), dwBytesRead);
-        printf("%s\n", ReadBuffer);
-    }
-    else if (dwBytesRead == 0)
-    {
-        _tprintf(TEXT("No data read from file COM6\n"));
-    }
-    else
-    {
-        printf("\n ** Unexpected value for dwBytesRead ** \n");
-    }*/
-
-    /*
-    //mask event
-    fSuccess = SetCommMask(hComm, EV_CTS | EV_DSR);
-
-    if (!fSuccess) {
-        printf("SetCommMask failed with error %d.\n", GetLastError());
-        return GetLastError();
-    }
-    //event object for use by WaitCommEvent
-    o.hEvent = CreateEvent(
-        NULL,
-        TRUE,
-        FALSE,
-        NULL);
-
-    //initialize OVERLAPPED structure to zero
-    o.Internal = 0;
-    o.InternalHigh = 0;
-    o.Offset = 0;
-    o.OffsetHigh = 0;
-
-    assert(o.hEvent);
-
-    if (WaitCommEvent(hComm, &dwEvtMask, &o))
-    {
-        if (dwEvtMask & EV_DSR)
-        {
-            // To do.
-            printf("DSR\n");
-        }
-
-        if (dwEvtMask & EV_CTS)
-        {
-            // To do. 
-            printf("CTS\n");
-        }
-    }
-    else
-    {
-        DWORD dwRet = GetLastError();
-        if (ERROR_IO_PENDING == dwRet)
-        {
-            printf("I/O is pending...\n");
-
-            // To do.
-        }
-        else
-            printf("Wait failed with error %d.\n", GetLastError());
-    }
-    */
+    
     CloseHandle(hComm); //closes the COM port
 
     return 0;
